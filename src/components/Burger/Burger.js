d2167ff8d0f6cba0cab2bb30998fc08dd9cfa51b
David <>
2021-01-19 13:46:06 +0700

    burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = ( props ) => {
-    let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
+const burger = (props) => {
+  //   let transformedIngredients = Object.keys(props.ingredients)
+  //     .map((igKey) => {
+  //       return [...Array(props.ingredients[igKey])].map((_, i) => {
+  //         return <BurgerIngredient key={igKey + i} type={igKey} />;
+  //       });
+  //     })
+  //     .reduce((arr, el) => {
+  //       return arr.concat(el);
+  //     }, []);
+
+  //   if (transformedIngredients.length === 0) {
+  //     transformedIngredients = <p>Please start adding ingredients!</p>;
+  //   }
+
+  let element = Object.keys(props.ingredients)
+    .map((e) => {
+      return [...Array(props.ingredients[e])].map((_, i) => {
+        return <BurgerIngredient key={e} type={e} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+    if(element.length < 1){
+        element=<p>Please start adding ingredients !</p>
     }
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {element}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,23 +4,20 @@ import Aux from '../../hoc/Aux1';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
     state = {
         ingredients: {
             salad: 0,
-            bacon: 0,
+            tomato: 0,
             cheese: 0,
             meat: 0
         }
     }
 
     render () {
+        const {ingredients} = this.state
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients} />
+                <Burger ingredients={ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
